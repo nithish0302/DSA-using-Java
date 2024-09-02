79aa69a82f1239c2936fc926999fad376fc20d8b
nithish0302 <nithi2005palani@gmail.com>
2024-09-02 14:51:29 +0530

    File added

---
+++ src/JAVA_DSA/Student_grade.java
@@ -0,0 +1,33 @@
+package JAVA_DSA;
+
+import java.util.Scanner;
+
+public class Student_grade {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Student Grade");
+        int score=sc.nextInt();
+        if(score>=90)
+        {
+            System.out.println("Student grade is A");
+           
+        }
+        else if(score>=80 && score<=89)
+        {
+            System.out.println("Student grade is B");
+        }
+        else if(score>=70 && score<=79)
+        {
+            System.out.println("Student grade is C");
+        }
+        else if(score>=60 && score<=69)
+        {
+            System.out.println("Student grade is D");
+        }
+        else
+        {
+            System.out.println("Student grade is F");
+        }
+        sc.close();
+    }
+}

45f23bf37f9341a82f89295990eb7bf90d1d048f
nithish0302 <nithi2005palani@gmail.com>
2024-09-13 10:16:29 +0530

    File added

---
+++ src/DAY11_JAVA_DSA/GameOfXOR.java
@@ -0,0 +1,51 @@
+package DAY11_JAVA_DSA;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class GameOfXOR {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Enter the array size");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter the array element");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+         //method 1
+        int totalxor = 0;
+        // List<Integer>li=new ArrayList<Integer>();
+        for (int i = 0; i < n; i++) {
+            totalxor ^= arr[i];
+            int sum = arr[i];
+            for (int j = i + 1; j < n; j++) {
+                sum ^= arr[j];
+                totalxor ^= sum;
+            }
+
+        }
+        System.out.println("The answer is");
+        System.out.println(totalxor);
+
+        //method 2
+        int xo=0;
+        int tot=0
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i;j<n;j++)
+            {
+                
+                for(int k=i;k<=j;k++)
+                {
+                    
+                }
+            }
+        }
+
+    }
+}

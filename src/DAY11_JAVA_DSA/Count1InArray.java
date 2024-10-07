fd0ec9626363254f1ba9043828e26321d650898a
nithish0302 <nithi2005palani@gmail.com>
2024-10-07 09:23:19 +0530

    File added

--- src/DAY11_JAVA_DSA/Count1InArray.java
+++ src/DAY11_JAVA_DSA/Count1InArray.java
@@ -1,26 +1,26 @@
 package DAY11_JAVA_DSA;
+
 import java.util.*;
+
 public class Count1InArray {
-    static Scanner sc=new Scanner(System.in);
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
         System.out.println("Enter the n size");
-        int n=sc.nextInt();
-        int arr[]=new int[n];
+        int n = sc.nextInt();
+        int arr[] = new int[n];
         System.out.println("Enter the array element");
-        for(int i=0;i<n;i++)
-        {
-            arr[i]=sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
         }
     }
-    static int binary(int arr[],int low,int high)
-    {
-        int val=0;
-        if(low<high)
-        {
-            int mid=(low+high)/2;
-            if(mid==1)
-            {
-                
+
+    static int binary(int arr[], int low, int high) {
+
+        if (low < high) {
+            int mid = (low + high) / 2;
+            if (mid == 1) {
+
             }
         }
         return 0;
--- src/DAY11_JAVA_DSA/GameOfXOR.java
+++ src/DAY11_JAVA_DSA/GameOfXOR.java
@@ -1,7 +1,5 @@
 package DAY11_JAVA_DSA;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class GameOfXOR {

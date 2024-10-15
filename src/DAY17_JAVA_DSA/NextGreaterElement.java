bc790ed1683cd527a838f23ebb88cee777a6446b
nithish0302 <nithi2005palani@gmail.com>
2024-10-15 10:37:29 +0530

    File added

--- src/DAY17_JAVA_DSA/NextGreaterElement.java
+++ src/DAY17_JAVA_DSA/NextGreaterElement.java
@@ -1,5 +1,7 @@
 package DAY17_JAVA_DSA;
+
 import java.util.*;
+
 /**
  * NextGreaterElement
  */
@@ -7,35 +9,34 @@ public class NextGreaterElement {
 
     public static void main(String[] args) {
         int n;
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of arr");
-        n=sc.nextInt();
+        n = sc.nextInt();
         System.out.println("Enter the array Element");
-        int arr[]=new int[n];
-        for(int i=0;i<n;i++)
-        arr[i]=sc.nextInt();
-        int res[]=nextgreaterelement(arr);
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        int res[] = nextgreaterelement(arr);
         System.out.println(Arrays.toString(res));
-
+        sc.close();
     }
-    public static int[] nextgreaterelement(int arr[])
-    {
-        int n=arr.length;
-        int res[]=new int[n];
+
+    public static int[] nextgreaterelement(int arr[]) {
+        int n = arr.length;
+        int res[] = new int[n];
         Stack<Integer> st = new Stack<>();
-        for(int i=0;i<n;i++)
-        {
-            while(!st.isEmpty() &&st.peek()<=arr[i])
-            {
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && st.peek() <= arr[i]) {
                 st.pop();
             }
-            if(st.isEmpty())
-            res[i]=-1;
+            if (st.isEmpty())
+                res[i] = -1;
             else
-            res[i]=st.peek();
+                res[i] = st.peek();
 
             st.push(arr[i]);
         }
+
         return res;
     }
 }
